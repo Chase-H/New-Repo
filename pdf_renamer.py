074745b5d114ddecdb5478625af6b26b6b80bcce
Chase-H <chase.hudson@ankura.com>
2018-10-25 17:54:38 -0400

    CH - Updated PDF Renamer

--- pdf_renamer.py
+++ pdf_renamer.py
@@ -1,35 +1,28 @@
-from StringIO import StringIO
-import pyPdf
+import PyPDF2
+import shutil
+import csv
 import os
 import re
 
-# a function here
-def getPDFContent(path):
-        content = ""
-            num_pages = 10
-                p = file(path, "rb")
-                    pdf = pyPdf.PdfFileReader(p)
-                        for i in range(0, num_pages):
-                                    content += pdf.getPage(i).extractText() + "\n"
-                                            content = " ".join(content.replace(u"\xa0", " ").strip().split())     
-                                                p.close()
-                                                    return content  
+original_directory = '/mnt/c/Users/chase.hudson/Documents/GitHub/New_Repo/pdfs/original'
+result_directory = '/mnt/c/Users/chase.hudson/Documents/GitHub/New_Repo/pdfs/result'
+logfile = "log.csv"
 
-                                                # name of the source PDF file
-                                                PDF_name = 'awsdfadsf '
+rename_data = [['Original_File', 'Renamed_File']]
+for subdir, dirs, files in os.walk(original_directory):
+    for file in files:
+        filepath = os.path.join(original_directory, file)
+        with open(filepath, 'rb') as pdfFileObj:
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj).getPage(0).extractText()
+            result = re.search(r'\d{9}', str(pdfReader))
+        if result:
+            new_filepath = os.path.join(original_directory, result.group(0) + '-' + file)
+            rename_data.append([filepath, new_filepath])
+            os.rename(filepath, new_filepath)
+        else:
+            rename_data.append([filepath, 'Claim Number Not Found'])
 
-                                                # picking texts from the PDF file
-                                                pdfContent = StringIO(getPDFContent(r"c:\Users\chase.hudson\Desktop\Email Project" + PDF_name + ".pdf").encode("ascii", "ignore"))
-                                                for line in pdfContent:
-                                                        aaa = line.find(r'\d{9}')
-
-                                                            final_name = str(aaa)
-
-                                                            print(final_name)
-
-                                                            f = open(r"c:\Users\chase.hudson\Desktop\Email Project" + PDF_name + ".pdf")
-                                                            f.close()
-
-                                                            os.rename(r"c:\Users\chase.hudson\Desktop\Email Project" + PDF_name + ".pdf", "C:\\" + final_name + ".pdf")
-
-
+with open(logfile, "w") as csv_file:
+    writer = csv.writer(csv_file)
+    for line in rename_data:
+        writer.writerow(line)
